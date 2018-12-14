4ebb6c8af9de438a17981d661313dac9b16dcfbe
Jasper Coekaerts <jaspercoekaerts@gmail.com>
2018-12-14 09:46:39 +0100

    Added Recourcetype + inst

--- main.erl
+++ main.erl
@@ -5,5 +5,3 @@ test() -> {ok, Gpio17} = gpio:start_link(17, input),
 		gpio:read(Gpio17),
 		gpio:register_int(Gpio17),
 		gpio:set_int(Gpio17, both).
-		
-get_time() -> calendar:now_to_local_time(erlang:timestamp()).
