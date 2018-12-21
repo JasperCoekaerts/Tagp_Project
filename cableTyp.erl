00233323b278b6ee579d6eccb62e445fddaa4a09
Luka Lageschaar <luka.lageschaar@gmail.com>
2018-12-21 10:18:43 +0100

    get and set R in cableTyp

--- cableTyp.erl
+++ cableTyp.erl
@@ -1,11 +1,17 @@
 -module(cableTyp).
--export([create/0]).
+-export([create/1]).
 
--export([loop/0, init/0]).
+-export([loop/1, init/1]).
 
-create() -> {ok, spawn_link(?MODULE, init, [])}.
-init() -> survivor:entry(cableTyp_created), loop().
+-export([create_test/0,init_test/1, setResistance/2, getResistance/1]).
 
+create(State) -> {ok, spawn_link(?MODULE, init, [State])}.
+init(State) -> survivor:entry(cableTyp_created), loop(State).
+
+
+create_test() -> {ok, spawn_link(?MODULE, init_test, [{3, 'Copper', 'Circle', 0.2, 230, 0.016, 3.68, {2, 1000}}])}.
+init_test(State) -> survivor:entry(test_cableTyp_created), 
+		    loop(State).
 % Things that define a cable(TypeOptions):
 	% Resistance in ohms
 	% material: Cu, Al, Sn, Au, Ag
@@ -17,9 +23,9 @@ init() -> survivor:entry(cableTyp_created), loop().
 		% surface area of section mm^2
 		% length in mm
 		
-	%TypeOptions = {Resistance, Material, CrossSectionShape, InsulationWidth, Umax, Imax, Pmax, {SectionArea, Length}}}
+	%State = {Resistance(ohms), Material, CrossSectionShape, InsulationWidth(mm), Umax(V), Imax(A), Pmax(W), {SectionArea(mm^2), Length(mm)}}}
 
-loop() ->
+loop(State) ->
     receive
 	{initial_state, [ResInst_Pid, TypeOptions], ReplyFn} ->
 	    Location = location:create(ResInst_Pid, emptySpace),
@@ -27,11 +33,23 @@ loop() ->
 	    Out = connector:create(ResInst_Pid, cable),
 	    ReplyFn(#{resInst => ResInst_Pid, chambers => [Location], 
 	    cList => [In, Out], typeOptions => TypeOptions}),
-	    loop();
+	    loop(State);
 	{connections_list, State , ReplyFn} -> 
 	    #{cList := C_List} = State, ReplyFn(C_List), 
-	    loop();
+	    loop(State);
 	{locations_list, State, ReplyFn} -> 
 	    #{chambers := L_List} = State, ReplyFn(L_List),
-	    loop()
+	    loop(State);
+	{get_resistance, ReplyFn} -> 
+		ReplyFn(element(1, State)),
+		loop(State);
+	{set_resistance, Resistance} ->
+	    State2 = setelement(1, State, Resistance),
+	    loop(State2)
     end. 
+    
+setResistance(Cable_Pid, Resistance) ->  Cable_Pid ! {set_resistance, Resistance}.
+getResistance(Cable_Pid) ->
+	msg:get(Cable_Pid, get_resistance).
+	
+	
