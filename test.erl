e2659542ffd51271f3afe13010e40c776d1d851b
Jasper Coekaerts <jaspercoekaerts@gmail.com>
2019-01-16 10:26:33 +0100

    New Test

--- test.erl
+++ test.erl
@@ -1,5 +1,5 @@
 -module(test).
--export([start/0,createConnection/0, test_conn/0]).
+-export([start/0,createConnection/0, test_conn/0, test_circuit/0]).
 
 start() -> 
 	survivor:start(),
@@ -25,3 +25,14 @@ test_conn() ->
     {ok, Source_I} = resourceInst:create(sourceInst,[self(), Source_T, Cable_I]),
     {ok,[C,D]} = resourceInst:list_connectors(Cable_I),
     connector:connect(C,Source_I).
+	
+test_circuit() ->
+	{ok, Kabel} = resourceType:create(cableTyp, [{1,2,3,4,5,6,7,{8,9}}]),
+	{ok, Bron} = resourceType:create(sourceTyp, [{100, 50, direct}]),
+	{ok, Gebruiker} = resourceType:create(deviceTyp, [{100, 10, 0, 230, 1000, 2, off}]),
+	{ok,Kabel_1} = resourceInst:create(cableInst, [self(), Kabel]),
+	{ok, Bron_1} = resourceInst:create(sourceInst, [self(), Bron, Kabel_1]),
+	{ok, Gebruiker_1} = resourceType:create(deviceTyp, [self(), Gebruiker, Kabel_1]),
+	{ok, [C1, C2]} = resourceInst:list_connectors(Kabel_1),
+	connector:connect(C1, Bron_1),
+	connector:connect(C2, Gebruiker_1).
