3905cc873e68cc33ece099893213ba0a4719c107
Jasper Coekaerts <jaspercoekaerts@gmail.com>
2019-01-16 14:33:47 +0100

    Changes made

--- test.erl
+++ test.erl
@@ -51,9 +51,25 @@ test_circuit() ->
 	survivor:entry({connection_Gebruiker, connector:get_connected(C4)}).
 	
 test_room() -> 
-	{ok, Kabel} = resourceType:create(cableTyp, [{0,'Copper','Circle',0.2,230,0.016,3.68,{0.5,1000}}]),
-	{ok, Bron} = resourceType:create(sourceTyp, [{100, 50, direct}]),
-	{ok, Gebruiker} = resourceType:create(deviceTyp, [{100, 10, 0, 230, 1000, 2, off}]),
-	{ok, Schakelaar} = resourceType:create(switchTyp, [{on_off, 100}]),
-
+	{ok, Kabel_frigo} = resourceType:create(cableTyp, [{0,'Copper','Circle',0.2,230,0.016,3.68,{0.5,3000}}]),
+	{ok, Kabel_microgolf} = resourceType:create(cableTyp, [{0,'Copper','Circle',0.2,230,0.016,3.68,{0.5,3000}}]),
+	{ok, Kabel_oven} = resourceType:create(cableTyp, [{0,'Copper','Circle',0.2,230,0.016,3.68,{0.5,3000}}]),
+	{ok, Kabel_vuur} = resourceType:create(cableTyp, [{0,'Copper','Circle',0.2,230,0.016,3.68,{0.5,3000}}]),
+	{ok, Kabel_dampkap} = resourceType:create(cableTyp, [{0,'Copper','Circle',0.2,230,0.016,3.68,{0.5,5000}}]),
+	{ok, Kabel_verwarming} = resourceType:create(cableTyp, [{0,'Copper','Circle',0.2,230,0.016,3.68,{0.5,6500}}]),
+	{ok, Kabel_lamp1} = resourceType:create(cableTyp, [{0,'Copper','Circle',0.2,230,0.016,3.68,{0.5,4500}}]),
+	{ok, Kabel_lamp2} = resourceType:create(cableTyp, [{0,'Copper','Circle',0.2,230,0.016,3.68,{0.5,7500}}]),
+	{ok, Kabel_lamp3} = resourceType:create(cableTyp, [{0,'Copper','Circle',0.2,230,0.016,3.68,{0.5,8000}}]),
+	{ok, Kabel_lamp4} = resourceType:create(cableTyp, [{0,'Copper','Circle',0.2,230,0.016,3.68,{0.5,11000}}]),
+	
+	{ok, Bron} = resourceType:create(sourceTyp, [{50, 230, direct}]),
+	{ok, Lamp} = resourceType:create(deviceTyp, [{60, 0, 0, 230, 16, 2, off}]),
+	{ok, Schakelaar} = resourceType:create(switchTyp, [{on_off, 0}]),
+	{ok, Frigo} = resourceType:create(deviceTyp, [{1000,200,0,230,0.8,1,on}]), %geen flauw idee wat voor waarden geschikt zijn voor R L en C
+	{ok, Microgolf} = resourceType:create(deviceTyp,[{1000,200,0,230,700,1,off}]),
+	{ok, Oven} = resourceType:create(deviceTyp,[{1000,200,0,230,2500,1,on}]),
+	{ok, Vuur} = resourceType:create(deviceTyp,[{1000,200,0,230,6000,1,on}]),
+	{ok, Dampkap} = resourceType:create(deviceTyp,[{1000,200,0,230,205,1,on}]),
+	{ok, Verwarming} = resourceType:create(deviceTyp,[{1000,200,0,230,1000,1,on}]),
+	
 	
